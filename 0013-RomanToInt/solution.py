cc1e665f10bfaab7c42cf685c75220e7fa12181d
mrrwhoo1 <maronchilomoschool@gmail.com>
2026-07-08 18:23:48 +0200

    0013 fixed to match leetcode style

--- 0013-RomanToInt/solution.py
+++ 0013-RomanToInt/solution.py
@@ -1,27 +1,31 @@
-characters = {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000,
-}
+class Solution:
+    def romanToInt(self, s: str) -> int:
 
-user = input("enter character[s]: ")
-new_list = list(user)
-total = 0
-for index, char in enumerate(new_list):
-    current_val = characters[char]
-    next_index = index + 1
+        self.characters = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000,
+        }
 
-    if next_index < len(new_list):
-        next_char = new_list[next_index]
-        next_val = characters[next_char]
+        self.new_list = list(s)
+        self.total = 0
+        for index, char in enumerate(self.new_list):
+            current_val = self.characters[char]
+            next_index = index + 1
 
-        if current_val < next_val:
-            total -= current_val
-        else:
-            total += current_val
-    else:
-        total += current_val
+            if next_index < len(self.new_list):
+                next_char = self.new_list[next_index]
+                next_val = self.characters[next_char]
+
+                if current_val < next_val:
+                    self.total -= current_val
+                else:
+                    self.total += current_val
+            else:
+                self.total += current_val
+
+        return self.total
