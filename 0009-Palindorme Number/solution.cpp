e64c7d0fd9b3d530ce00ef39d8e663e6e56226a8
mrrwhoo <mrrwhoo@mrrwhoo.mrrwhoo>
2026-07-08 15:36:49 +0200

    forgot to clean up the cpp file

--- 0009-Palindorme Number/solution.cpp
+++ 0009-Palindorme Number/solution.cpp
@@ -2,32 +2,17 @@
 #include <algorithm>
 #include <string>
 
-
-int main(){
+class Solution {
+public:
+    bool isPalindrome(int x) {
     int nums = 121;
     std::string str_original = std::to_string(nums);
     std::string converted = std::to_string(nums);
     std::reverse(converted.begin(), converted.end());
     
     if(str_original == converted) {
-        std::cout<<"True";}
-    else {std::cout<<"false";}
-
-
-    
-}
-
-// class Solution {
-// public:
-//     bool isPalindrome(int x) {
-//     int nums = 121;
-//     std::string str_original = std::to_string(nums);
-//     std::string converted = std::to_string(nums);
-//     std::reverse(converted.begin(), converted.end());
-    
-//     if(str_original == converted) {
-//         return true;}
-//     else {return false;}
+        return true;}
+    else {return false;}
         
-//     }
-// };
+    }
+};
