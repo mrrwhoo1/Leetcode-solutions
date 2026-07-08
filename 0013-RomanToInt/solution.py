8fe5c4555e28a9c17e8a773c4c72b47768146dee
mrrwhoo <mrrwhoo@mrrwhoo.mrrwhoo>
2026-07-08 18:10:57 +0200

    0013 py solution for py

--- 0013-RomanToInt/solution.py
+++ 0013-RomanToInt/solution.py
@@ -1,12 +1,11 @@
-
 characters = {
     "I": 1,
-    "V":5,
-    "X":10,
-    "L":50,
-    "C":100,
-    "D":500,
-    "M":1000,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000,
 }
 
 user = input("enter character[s]: ")
@@ -15,23 +14,14 @@ total = 0
 for index, char in enumerate(new_list):
     current_val = characters[char]
     next_index = index + 1
-    
-    if (next_index < len(new_list)):
+
+    if next_index < len(new_list):
         next_char = new_list[next_index]
         next_val = characters[next_char]
-        
-        if current_val<next_val:
+
+        if current_val < next_val:
             total -= current_val
         else:
             total += current_val
     else:
         total += current_val
-        
-        
-        
-        
-    
-        
-    
-
-    
