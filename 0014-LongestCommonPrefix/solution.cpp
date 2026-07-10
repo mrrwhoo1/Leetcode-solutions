84309db1fdb9f9b86b671bbf3ef91bb54ba46753
mrrwhoo1 <maronchilomoschool@gmail.com>
2026-07-10 06:06:15 +0200

    0014 c++ solution complete

--- 0014-LongestCommonPrefix/solution.cpp
+++ 0014-LongestCommonPrefix/solution.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
 #include <string>
 #include <vector>
+using namespace std;
 
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs)
     {
+        string first_name = strs[0];
+        for (int index = 1; index < strs.size(); index++) {
+            while (!strs[index].starts_with(first_name)) {
+                first_name = first_name.substr(0, first_name.length() - 1);
+            }
+        }
+        return first_name;
     }
 };
+
+int main()
+{
+    Solution obj;
+    vector<string> strs = { "maron", "martha", "martin" };
+    string result = obj.longestCommonPrefix(strs);
+    cout << result << endl;
+    return 0;
+}
